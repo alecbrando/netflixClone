57a12e8c8e06bd46f41ed671ed990222d594deae
alecbrando <37994059+alecbrando@users.noreply.github.com>
2021-04-13 15:22:58 -0700

    added sass styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
-import Row from './components/Row'
-import requests from './requests'
+import React from "react";
+import "./App.css";
+import Row from "./components/Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,40 +1,41 @@
-import React, {useState, useEffect} from 'react'
-import instance from '../axios'
-
-const baseUrl = 'https://image.tmdb.org/t/p/original/'
+import React, { useState, useEffect } from "react";
+import instance from "../axios";
+import styles from "../styles/row.module.scss";
+const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 export type RowPropTypes = {
-    title: string
-    fetchUrl: string
+  title: string;
+  fetchUrl: string;
+};
+
+function Row({ title, fetchUrl }: RowPropTypes) {
+  const [movies, setMovies] = useState<any>([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const request = await instance.get(fetchUrl);
+      setMovies(request.data.results);
+    };
+    getMovies();
+  }, [fetchUrl]);
+
+  console.table(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className={styles.row__posters}>
+        {movies.map((movie: any) => (
+          <img
+            key={movie.id}
+            className={styles.row__poster}
+            src={`${baseUrl}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-function Row({title, fetchUrl }: RowPropTypes) {
-    const [movies, setMovies] = useState<any>([])
-
-    
-
-    useEffect(() => {
-        const getMovies = async() => {
-            const request = await instance.get(fetchUrl)
-            setMovies(request.data.results)
-        }
-        getMovies()
-    }, [fetchUrl])
-
-
-
-    console.table(movies)
-
-    return (
-        <div className="row">
-            <h2>{title}</h2>
-            <div className="row_posters" style={{'flex-direction: row'}}>
-                {movies.map((movie: any) => (
-                    <img className="row__poster" src={`${baseUrl}${movie.poster_path}`} alt={movie.name} />
-                ))}
-            </div>
-        </div>
-    )
-}
-
-export default Row
+export default Row;
