8fa930e529a0a64d3deecd9ab45102a40f2b35e1
alecbrando <37994059+alecbrando@users.noreply.github.com>
2021-04-13 19:24:30 -0700

    added youtube video when clicking thumbnail;

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
 import instance from "../axios";
 import styles from "../styles/row.module.scss";
+// @ts-ignore
+import movieTrailer from "movie-trailer";
+
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 export type RowPropTypes = {
@@ -11,6 +15,7 @@ export type RowPropTypes = {
 
 function Row({ title, fetchUrl, isLargeRow }: RowPropTypes) {
   const [movies, setMovies] = useState<any>([]);
+  const [trailerUrl, setTrailerUrl] = useState<any>();
   useEffect(() => {
     const getMovies = async () => {
       const request = await instance.get(fetchUrl);
@@ -19,12 +24,45 @@ function Row({ title, fetchUrl, isLargeRow }: RowPropTypes) {
     getMovies();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async (movie: any) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      if (movie?.name) {
+        movieTrailer(movie?.name || "")
+          .then((url: any) => {
+            const urlParams = new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get("v"));
+          })
+          .catch((error: Error) => console.error(error));
+      } else {
+        movieTrailer(movie?.title || "")
+          .then((url: any) => {
+            console.log(url);
+            const urlParams = new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get("v"));
+          })
+          .catch((error: Error) => console.error(error));
+      }
+    }
+  };
+
   return (
     <div className={styles.row}>
       <h2>{title}</h2>
       <div className={styles.row__posters}>
         {movies.map((movie: any) => (
           <img
+            onClick={() => handleClick(movie)}
             key={movie.id}
             className={
               isLargeRow ? styles.row__posterLarge : styles.row__poster
@@ -36,6 +74,14 @@ function Row({ title, fetchUrl, isLargeRow }: RowPropTypes) {
           />
         ))}
       </div>
+
+      {trailerUrl && (
+        <YouTube
+          videoId={trailerUrl}
+          //@ts-ignore
+          opts={opts}
+        />
+      )}
     </div>
   );
 }
